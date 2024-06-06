4aeb9a4e5dbe60b9e3ea2a597305e053fdf51aff
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-06 07:06:18 +0100

    2 more demons excorcised.

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,18 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2
+    case '-':
+      return num1 - num2
+    case '*':
+      return num1 * num2
+    case '/':
+      return num1 / num2
+    default:
+      throw new Error(
+        `You've passed an invalid operator of ${operator}, I only accept +, -, *, or /.`
+      )
+  }
+}
 
-module.exports = calculator;
+module.exports = calculator
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,20 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  // ------ attempt 1 ------
+  // let count = 0
 
-module.exports = countOccurrences;
+  // for (const character of str) {
+  //   character === char && count++
+  // }
+
+  // return count
+
+  // ------ attempt 2 ------
+  return str.split(char).length - 1
+  // str.split(char) splits string into an array of substrings seperated by char
+  // .length gives number of parts string was split into
+  // -1 gives number of occurences of char
+  // For the string "hello" and the character "l", str.split("l") would result in ["he", "", "o"], which has a length of 3. The number of occurrences of "l" is length - 1, which is 3 - 1 = 2.
+  // For the string "banana" and the character "a", str.split("a") would result in ["b", "n", "n", ""], which has a length of 4. The number of occurrences of "a" is length - 1, which is 4 - 1 = 3.
+}
+
+module.exports = countOccurrences

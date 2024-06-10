2f6e7be351e1db531542d5544c69cd27d6cf2cb2
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-10 18:54:57 +0100

    oh boy

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,23 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  // cheating
+  // return Math.max(...arr)
 
-module.exports = findMaxNumber;
+  // errrror handler
+  if (arr.length === 0)
+    throw new Error(
+      "Hmm. Looks like you've asked me to find the max item within a collection of nothing.. what the heck?!"
+    )
+
+  // brute force
+  let maxNumber = arr[0]
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxNumber) {
+      maxNumber = arr[i]
+    }
+  }
+
+  return maxNumber
+}
+
+module.exports = findMaxNumber

7c74a15a1b8fdf3fdac28349bf0f6d398052a3a7
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-10 19:32:15 +0100

    regex!

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(str) {
+  // regex, regex, regex!
+  return str
+    .toLowerCase()
+    .replace(/(?:^\w[A-Z]|\b\w)/g, (firstLetterOfWord) => {
+      return firstLetterOfWord.toUpperCase()
+    })
+}
 
-module.exports = titleCase;
+module.exports = titleCase
+
+// (?:^\w|\b\w): This non-capturing group contains two alternatives:
+// ^\w: Match the first word character at the beginning of the string.
+// \b\w: Match any word character at a word boundary - the first character after a space
+// /g: The global flag, which tells the regex engine to find all matches in the string, not just the first one.

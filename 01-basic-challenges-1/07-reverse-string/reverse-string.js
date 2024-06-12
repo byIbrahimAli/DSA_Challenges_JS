e11cdf54e6276f8991d7683b8c14976f8bb2af72
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-12 20:57:27 +0100

    apple intelligence news came today

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,18 @@
-function reverseString() {}
+function reverseString(str) {
+  // my way
+  let reverseStr = str[str.length - 1]
 
-module.exports = reverseString;
+  for (let i = str.length - 2; i >= 0; i--) {
+    reverseStr += str[i]
+  }
+
+  return reverseStr
+
+  // one liner
+  // return str.split('').reverse().join('');
+  // array split by each char
+  // array.reverse()
+  // convert array to string
+}
+
+module.exports = reverseString

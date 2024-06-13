c1caad1d005c9b1b9c95de3881d76e9cb8b1b6d7
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-13 22:42:15 +0100

    pqlindoqme

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+const isPalindrome = (str) => sanitiseString(str) === reverseString(str) // str and reverse(str) the same ? T : F
 
-module.exports = isPalindrome;
+const reverseString = (str) => sanitiseString(str.split('').reverse().join('')) // reverse sanitised string
+
+const sanitiseString = (str) => str.toLowerCase().replace(/[^a-z0-9]/g, '') // lowercase, chars and numbers only - no spaces, symbols
+
+module.exports = isPalindrome
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,37 @@
-function countVowels() {}
+// brute force
+function countVowels(str) {
+  let count = 0
 
-module.exports = countVowels;
+  for (const character of str) {
+    if (isVowel(character)) count++
+  }
+
+  return count
+}
+
+const isVowel = (char) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+
+  for (let i = 0; i < vowels.length; i++) {
+    if (vowels[i] === char.toLowerCase()) return true
+  }
+
+  return false
+}
+
+// ✋🛑 brute force is 🐌
+// loop: O(n) where n is no of vowels
+// set: O(1) cos of hash-based lookups
+function countVowelsSpeed(str) {
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+  let count = 0
+
+  for (const character of str) {
+    if (vowels.has(character.toLowerCase())) count++
+    // .has() is way faster than looping through vowels O(1) > O(n)
+  }
+
+  return count
+}
+
+module.exports = countVowelsSpeed

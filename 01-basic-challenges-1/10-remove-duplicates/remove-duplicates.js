a08b79729649debffaba3525e7dcc23ccd4d80ae
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2024-06-23 22:47:53 +0100

    1 basic challenges done

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,17 @@
-function removeDuplicates() {}
+function removeDuplicatesForLoop(arr) {
+  let newArr = []
 
-module.exports = removeDuplicates;
+  for (item of arr) {
+    if (!newArr.includes(item)) {
+      newArr.push(item)
+    }
+  }
+
+  return newArr
+}
+
+function removeDuplicatesSet(arr) {
+  return [...new Set(arr)]
+}
+
+module.exports = removeDuplicatesForLoop
